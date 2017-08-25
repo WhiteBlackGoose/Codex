2f3b0e7e18ba92a7c76898ac2251123bd8f8bb7a
Lance Collins <lancec@microsoft.com>
2017-08-25 10:28:57 -0700

    Rename CodeGen to Types

--- src/Codex.Framework.Types/ICodeReview.cs
+++ src/Codex.Framework.Types/ICodeReview.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,7 +47,6 @@ namespace Codex
         Approved,
         Declined
     }
-
 
     public interface ICodeReviewerInfo
     {
