e6f055b732710bad7f30c7ddf5aaacc568246d0c
Lance Collins <lancec@microsoft.com>
2017-09-09 12:47:36 -0700

    Add test case and verify no bug for base interfaces in type params of base types.

---
+++ src/TestProjects/CodexTestRepo/CodexTestCSharpLibrary/Cases.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodexTestCSharpLibrary
+{
+    public interface IXedoc
+    {
+
+    }
+
+    public interface ITypeWithParams<T0, T1>
+    {
+    }
+
+    // Test interface implementations only applies to actual base types and not those which appear as type parameters 
+    public interface XedocImpl : IXedoc, ITypeWithParams<IXedoc, string>
+    {
+
+    }
+}
