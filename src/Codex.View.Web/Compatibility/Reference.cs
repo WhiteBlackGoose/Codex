f7898f10ab5c831145daed18b2772d709023c5b6
Lance Collins <lancec@microsoft.com>
2017-10-18 08:45:41 -0700

    Add in initial view projects

---
+++ src/Codex.View.Web/Compatibility/Reference.cs
@@ -0,0 +1,15 @@
+﻿using Granular.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codex.View.Web.Compatibility
+{
+    class Reference
+    {
+        // Reference host project in order to ensure it included
+        private static HtmlValueConverter HostReference;
+    }
+}
