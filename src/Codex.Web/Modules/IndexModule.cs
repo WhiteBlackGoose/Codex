b892ffe0d014ccb7989be2f10ae7d3fef87f4778
Lance Collins <lancec@microsoft.com>
2017-11-04 14:16:22 -0700

    More cleanup.

---
+++ src/Codex.Web/Modules/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Codex.Web.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => View["index"];
+        }
+    }
+}
