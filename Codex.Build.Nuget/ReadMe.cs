73acd277252a855cb744d96418aa3d92fc16f92a
Lance Collins <lancec@microsoft.com>
2018-01-06 08:11:18 -0800

    Adding project for producing Nuget package. Unfinished.

---
+++ Codex.Build.Nuget/ReadMe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Codex.Build.Nuget
+{
+    /// <summary>
+    /// This project is only intended to wrap dependencies for using Codex during build from nuget package
+    /// </summary>
+    public class ReadMe
+    {
+    }
+}
