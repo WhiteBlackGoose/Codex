dbcaca5e5dec9b56156cf1f55d6731361603e457
Lance Collins <lancec@microsoft.com>
2018-06-08 07:36:26 -0700

    A little more work on id registry.

--- src/Codex.ElasticSearch/Store/ElasticSearchIdRegistry.cs
+++ src/Codex.ElasticSearch/Store/ElasticSearchIdRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,6 +7,17 @@ namespace Codex.ElasticSearch
 {
     public class ElasticSearchIdRegistry : IStableIdRegistry
     {
+        private readonly ElasticSearchStore store;
+        private readonly ElasticSearchEntityStore<IStableIdMarker> idStore;
+        private readonly ElasticSearchService service;
+
+        public ElasticSearchIdRegistry(ElasticSearchStore store)
+        {
+            this.store = store;
+            this.service = store.Service;
+            this.idStore = store.StableIdMarkerStore;
+        }
+
         public Task SetStableIdsAsync(IReadOnlyList<IStableIdItem> items)
         {
             // For each item, reserve ids from the {IndexName}:{StableIdGroup} StableIdMarker document and assign tentative ids for each
