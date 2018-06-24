a13454e329898ea2583caa1d53e31ce7ea7d9b44
Lance Collins <lancec@microsoft.com>
2018-06-24 09:09:44 -0700

    Add modes to Codex.Automation.Workflow.

--- src/Codex.Automation.Workflow/Program.cs
+++ src/Codex.Automation.Workflow/Program.cs
@@ -18,6 +18,14 @@ namespace Codex.Automation.Workflow
         public string SourcesDirectory;
         public string RepoName;
     }
+
+    enum Mode
+    {
+        Prepare = 1 << 0,
+        AnalyzeOnly = 1 << 1,
+        FullAnalyze = Prepare | AnalyzeOnly
+    }
+
     class Program
     {
         private static Arguments ParseArguments(string[] args)
@@ -67,58 +75,101 @@ namespace Codex.Automation.Workflow
             return false;
         }
 
+        private static Mode GetMode(ref string[] args)
+        {
+            if (args[0].StartsWith("/"))
+            {
+                // No mode specified. Use default mode.
+                return Mode.FullAnalyze;
+            }
+            else
+            {
+                var modeArgument = args[0];
+                args = args.Skip(1).ToArray();
+
+                if (!Enum.TryParse<Mode>(modeArgument, ignoreCase: true, result: out var mode))
+                {
+                    throw new ArgumentException("Invalid mode: " + modeArgument);
+                }
+
+                return mode;
+            }
+        }
+
+        private static bool HasModeFlag(Mode mode, Mode flag)
+        {
+            return (mode & flag) == flag;
+        }
+
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                // TODO: Add help text
+                Console.WriteLine("No arguments specified.");
+                return;
+            }
+
+            Mode mode = GetMode(ref args);
+
             Arguments arguments = ParseArguments(args);
 
-            // download files
-            var client = new WebClient();
-            string downloadDirectory = Path.Combine(arguments.CodexOutputRoot, "zip");
-            string zipPath = Path.Combine(downloadDirectory, "Codex.zip");
-            Directory.CreateDirectory(downloadDirectory);
-
-            Console.WriteLine("Downloading Files...");
-            client.DownloadFile("https://github.com/Ref12/Codex/releases/download/latest-prerel/Codex.zip", zipPath);
-
-            // zip/unzip files
-            Console.WriteLine("Creating Directories");
-            string codexBin = Path.Combine(arguments.CodexOutputRoot, "bin");
-            Directory.CreateDirectory(codexBin);
-            Console.WriteLine("Extracting Zip Files");
-            ZipFile.ExtractToDirectory(zipPath, codexBin); // TODO doesnt work if the directory already exists?
-
-            // run exe
-            Console.WriteLine("Running Process");
-            string executablePath = Path.Combine(codexBin, "Codex.exe");
+            string codexBinDirectory = Path.Combine(arguments.CodexOutputRoot, "bin");
             string analysisOutputDirectory = Path.Combine(arguments.CodexOutputRoot, "store");
-            Process runExe = Process.Start(new ProcessStartInfo(executablePath, string.Join(" ",
-                "index",
-                "-save",
-                analysisOutputDirectory,
-                "-p",
-                arguments.SourcesDirectory,
-                "-repoUrl",
-                arguments.CodexRepoUrl,
-                "-n",
-                arguments.RepoName,
-                arguments.AdditionalCodexArguments))
+
+            if (HasModeFlag(mode, Mode.Prepare))
             {
-                UseShellExecute = false
-            });
-            runExe.WaitForExit();
+                // download files
+                var client = new WebClient();
+                string downloadDirectory = Path.Combine(arguments.CodexOutputRoot, "zip");
+                string zipPath = Path.Combine(downloadDirectory, "Codex.zip");
+                Directory.CreateDirectory(downloadDirectory);
 
-            // get json files and zip
-            Console.WriteLine("Zipping JSON files");
+                Console.WriteLine("Downloading Files...");
+                client.DownloadFile("https://github.com/Ref12/Codex/releases/download/latest-prerel/Codex.zip", zipPath);
 
-            string analysisOutputZip = analysisOutputDirectory + ".zip";
-            ZipFile.CreateFromDirectory(analysisOutputDirectory, analysisOutputZip);
+                // zip/unzip files
+                Console.WriteLine("Creating Directories");
+                Directory.CreateDirectory(codexBinDirectory);
+                Console.WriteLine("Extracting Zip Files");
+                ZipFile.ExtractToDirectory(zipPath, codexBinDirectory); // TODO: doesnt work if the directory already contains files
 
-            // publish to a vsts build
-            Console.WriteLine("Publishing to Build");
-            Console.WriteLine($"##vso[artifact.upload artifactname=CodexOutputs;]{analysisOutputZip}");
-            Console.WriteLine("##vso[build.addbuildtag]CodexOutputs");
+                Console.WriteLine($"##vso[task.setvariable variable=CodexBinDir;]{codexBinDirectory}");
+                Console.WriteLine($"##vso[task.setvariable variable=CodexAnalysisOutDir;]{analysisOutputDirectory}");
+            }
 
-            
+            if (HasModeFlag(mode, Mode.AnalyzeOnly))
+            {
+                // run exe
+                Console.WriteLine("Running Process");
+                string executablePath = Path.Combine(codexBinDirectory, "Codex.exe");
+                Process runExe = Process.Start(new ProcessStartInfo(executablePath, string.Join(" ",
+                    "index",
+                    "-save",
+                    analysisOutputDirectory,
+                    "-p",
+                    arguments.SourcesDirectory,
+                    "-repoUrl",
+                    arguments.CodexRepoUrl,
+                    "-n",
+                    arguments.RepoName,
+                    arguments.AdditionalCodexArguments))
+                {
+                    UseShellExecute = false
+                });
+                runExe.WaitForExit();
+
+                // get json files and zip
+                Console.WriteLine("Zipping JSON files");
+
+                string analysisOutputZip = analysisOutputDirectory + ".zip";
+                ZipFile.CreateFromDirectory(analysisOutputDirectory, analysisOutputZip);
+
+                // publish to a vsts build
+                Console.WriteLine("Publishing to Build");
+                Console.WriteLine($"##vso[artifact.upload artifactname=CodexOutputs;]{analysisOutputZip}");
+                Console.WriteLine("##vso[build.addbuildtag]CodexOutputs");
+            }
         }
     }
 }
