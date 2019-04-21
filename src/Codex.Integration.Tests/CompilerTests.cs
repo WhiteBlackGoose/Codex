c95a59948a512391be3fde35fc960e9c496b3152
Lance Collins <lancec@microsoft.com>
2019-04-20 18:28:20 -0700

    Fixes for build

---
+++ src/Codex.Integration.Tests/CompilerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codex.Integration.Tests
+{
+    [TestFixture]
+    class CompilerTests
+    {
+        [Test]
+        public void TestGetAssemblySymbol()
+        {
+            CSharpCompilation compilation = CSharpCompilation.Create("TestAssembly");
+            var reference = MetadataReference.CreateFromFile(@"D:\Code\CloudStore\bin\ContentStore\Distributed\Debug\Microsoft.ContentStore.Grpc.dll");
+            compilation = compilation.AddReferences(reference);
+
+            var assemblySymbol =compilation.GetAssemblyOrModuleSymbol(reference);
+        }
+    }
+}
