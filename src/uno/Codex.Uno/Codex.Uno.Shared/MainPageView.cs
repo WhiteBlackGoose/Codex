274f7383a89c49f8c0a744cdd23e7cdd82bf1cb9
Lance Collins <lancec@microsoft.com>
2020-11-14 13:49:39 -0800

    Before switching to code as UI.

---
+++ src/uno/Codex.Uno/Codex.Uno.Shared/MainPageView.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Uno.Extensions;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Codex.Uno.Shared
+{
+    using static ViewBuilder;
+
+    public class MainPageView
+    {
+        public static LinearGradientBrush PageHeaderBrush = new LinearGradientBrush()
+        {
+            StartPoint = new Point(0, 0),
+            EndPoint = new Point(1, 1),
+            GradientStops =
+            {
+                new GradientStop { Color = Color(0x744482), Offset = 0 },
+                new GradientStop { Color = Color(0x7db9e8), Offset = 0.8 },
+                new GradientStop { Color = Color(0xb7fff9), Offset = 1 },
+            }
+        };
+
+        public static FrameworkElement Create()
+        {
+            return new DockPanel().WithChildren(
+                Top
+                (
+                    new Grid()
+                    {
+                        Height = 58,
+                        Background = PageHeaderBrush,
+                        ColumnDefinitions =
+                        {
+                            // SearchBoxAndImagesColumn
+                            new ColumnDefinition() { Width = GridLength.Auto },
+                            new ColumnDefinition(),
+                            // HeaderMenuColumn
+                            new ColumnDefinition() { Width = GridLength.Auto }
+                        }
+                    }.WithChildren(
+                        new StackPanel { VerticalAlignment = VerticalAlignment.Stretch, Orientation = Orientation.Horizontal }.WithChildren(
+                            new TextBox()
+                            {
+                                FontFamily = new FontFamily("Arial"),
+                                FontSize = 21.333,
+                                Foreground = B(Colors.Black),
+                                Background = B(Colors.White),
+                                VerticalContentAlignment = VerticalAlignment.Center,
+                                MaxLength = 260,
+                                Width = 492,
+                                Padding = new Thickness(32, 5, 5, 5),
+                                Margin = new Thickness(8, 8, 0, 8),
+                            },
+                            new Border() { VerticalAlignment = VerticalAlignment.Center, Margin = new Thickness(10), Width = 95.5 }.WithChild(
+                                new Image() { Source = RelativeImageSource("Assets/Images/microsoftlogo.png") }
+                            ),
+                            new TextBlock()
+                            {
+                                Text = "Codex",
+                                Foreground = B(Colors.White),
+                                VerticalAlignment = VerticalAlignment.Center,
+                                FontSize = 20,
+                                Width = 56,
+                                Margin = new Thickness(0, 0, 0, 4)
+                            }
+                        )
+                    )
+                ),
+                new Grid()
+                {
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition() { Width = new GridLength(500) },
+                        new ColumnDefinition() { Width = GridLength.Auto },
+                        new ColumnDefinition()
+                    }
+                }
+
+            );
+        }
+    }
+}
---
+++ src/uno/Codex.Uno/Codex.Uno.Shared/ViewBuilders.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Codex.Uno.Shared
+{
+    public static class ViewBuilder
+    {
+        public static TPanel WithChildren<TPanel>(this TPanel panel, params PanelChild<TPanel>[] children)
+            where TPanel : Panel
+        {
+            foreach (var child in children)
+            {
+                child.AddToPanel(panel);
+            }
+
+            return panel;
+        }
+
+        public static ImageSource RelativeImageSource(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Border WithChild(this Border border, FrameworkElement element)
+        {
+            border.Child = element;
+            return border;
+        }
+
+        public static PanelChild<DockPanel> Top(FrameworkElement element)
+        {
+            DockPanel.SetDock(element, Dock.Top);
+            return element;
+        }
+
+        public static Color Color(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Brush B(Color color)
+        {
+            return new SolidColorBrush(color);
+        }
+    }
+
+    public class PanelChild<TPanel>
+        where TPanel : Panel
+    {
+        public FrameworkElement ChildElement { get; }
+
+        public PanelChild(FrameworkElement childElement)
+        {
+            ChildElement = childElement;
+        }
+
+        public virtual void AddToPanel(TPanel panel)
+        {
+            panel.Children.Add(panel);
+        }
+
+        public static implicit operator PanelChild<TPanel>(FrameworkElement childElement)
+        {
+            return new PanelChild<TPanel>(childElement);
+        }
+    }
+}
