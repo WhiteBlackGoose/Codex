1a7398687b2c4637865580c653c6b1b2b12fb033
Lance Collins <lancec@microsoft.com>
2020-12-23 03:26:44 -0800

    Records support.

---
+++ src/TestProjects/CodexTestRepo/CodexTestCSharpLibrary/Cases/Records.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodexTestCSharpLibrary
+{
+    public record Record1(int Prop);
+
+    public record RecordType(Record1 R1);
+}
+
+namespace System.Runtime.CompilerServices
+{
+    /// <nodoc />
+    public sealed class IsExternalInit
+    {
+    }
+}
